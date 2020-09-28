1d6898e9739334b63cf9a4382f7fd6bc8afa096b
Jairo Valderrama <jairovg@gmail.com>
2020-09-27 21:10:20 -0500

    refactor(storybook): add primary button story

---
+++ stories/components/Button.vue.stories.js
@@ -0,0 +1,25 @@
+import Button from '~/components/Button.vue';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    kind: {
+      control: {
+        type: 'inline-radio',
+        options: ['primary'],
+      },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { Button },
+  props: Object.keys(argTypes),
+  template: '<Button :kind="kind" />'
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  kind: 'primary',
+};
