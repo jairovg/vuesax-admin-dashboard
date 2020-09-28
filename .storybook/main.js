f639a817fbf09ae00c5018cb81b77164baab794a
Jairo Valderrama <jairovg@gmail.com>
2020-09-27 21:12:34 -0500

    fix(storybook): add alias to src folder in webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,20 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|mdx)'],
   addons: [
     '@storybook/addon-docs',
     '@storybook/preset-scss',
   ],
-}
+  webpackFinal: (config) => merge([
+    {
+      resolve: {
+        alias: {
+          '~': path.resolve(__dirname, '..', 'src'),
+        },
+      },
+    },
+    config,
+  ]),
+};
