179d249fdeaeff60cefa438b3df111d57b926324
Hernan Silva <hachesilva@gmail.com>
2020-10-20 14:44:28 -0500

    refactor: include style dictionary config and genereated tokens for sass and js

---
+++ src/assets/tokens.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 20 Oct 2020 19:43:06 GMT
+ */
+
+export const ColorSchemeBlack = "#000000";
+export const ColorSchemeWhite = "#ffffff";
+export const ColorSchemeOrange = "#ffa84c";
+export const ColorSchemeGray1 = "#2c2c2c";
+export const ColorSchemeGray2 = "#4f4f4f";
+export const ColorSchemeGray3 = "#979797";
+export const ColorSchemeGray4 = "#bdbdbd";
+export const ColorSchemeGray5 = "#ededed";
+export const ColorSchemeGray6 = "#f9f9f9";
+export const ColorSchemePurple1 = "#6f64f8";
+export const ColorSchemePurple2 = "#7e72f2";
+export const ColorTextButtonPrimary = "#ffffff";
+export const ColorBackgroundButtonPrimary = "#6f64f8";
