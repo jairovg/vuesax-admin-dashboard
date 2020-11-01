19ac901ac6bb540c80c231aa205ab76dafe2b0c3
Jairo Valderrama <jairovg@gmail.com>
2020-11-01 12:28:26 -0500

    build(eslint): ignore node_modules and build/lib

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ const { ESLINT_VUE_EXTENDS, ESLINT_VUE_PARSER } = require('webpack-lib').eslint;
 
 module.exports = {
   extends: ESLINT_VUE_EXTENDS,
+  ignorePatterns: [
+    '**/node_modules/**',
+    'build/lib/**',
+  ],
   parser: ESLINT_VUE_PARSER,
   parserOptions: {
     ecmaVersion: 6,
